d1818af93acc27ecb37874b37c2f9edb8a35fb58
Radhika Aggarwal <radhika142006@gmail.com>
2026-03-06 08:08:59 +0530

    Add postfix evaluation using linked list
    Implement postfix expression evaluation using a linked list stack.

---
+++ Day34que1.c
@@ -0,0 +1,76 @@
+/*Problem: Evaluate Postfix Expression - Implement using linked list with dynamic memory allocation.
+
+Input:
+- Postfix expression with operands and operators
+
+Output:
+- Print the integer result
+
+Example:
+Input:
+2 3 1 * + 9 -
+
+Output:
+-4
+
+Explanation:
+Use stack to store operands, apply operators by popping operands, push result back. Final stack top is result...without commment*/
+
+code:
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+struct node{
+    int data;
+    struct node* next;
+};
+
+struct node* top = NULL;
+
+void push(int x){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->next = top;
+    top = temp;
+}
+
+int pop(){
+    if(top == NULL) return 0;
+    struct node* temp = top;
+    int val = temp->data;
+    top = top->next;
+    free(temp);
+    return val;
+}
+
+int evaluate(char exp[]){
+    char *token = strtok(exp, " ");
+    while(token != NULL){
+        if(isdigit(token[0]) || (token[0]=='-' && isdigit(token[1]))){
+            push(atoi(token));
+        }else{
+            int b = pop();
+            int a = pop();
+            int res;
+            switch(token[0]){
+                case '+': res = a + b; break;
+                case '-': res = a - b; break;
+                case '*': res = a * b; break;
+                case '/': res = a / b; break;
+            }
+            push(res);
+        }
+        token = strtok(NULL, " ");
+    }
+    return pop();
+}
+
+int main(){
+    char exp[100];
+    fgets(exp, sizeof(exp), stdin);
+    exp[strcspn(exp, "\n")] = 0;
+    printf("%d", evaluate(exp));
+    return 0;
+}
